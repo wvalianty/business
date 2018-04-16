b7497900fa689d20437e82fa4f68edecd2bbbd22
root <root@DESKTOP-ETNKUVH.localdomain>
2018-04-16 15:53:26 +0800

    添加sql语句执行错误报错信息

--- www/controllers/settleApply.py
+++ www/controllers/settleApply.py
@@ -11,23 +11,51 @@ async def settleApply_index(*,keyword=None, page=1, pageSize=10):
     #if search
     if keyword:
         where = "name like '%%{}%%'".format(keyword)
-    total = await Income.findNumber('count(id)', where)
+    try:
+        total = await Income.findNumber('count(id)', where)
+    except:
+        ValueError("sql error:Income.findNumber('count(id)', where)")
+        return {
+            "page": (1,1),
+            "list": [{"id":"error"}]
+        }
     limit = ((page - 1) * pageSize, pageSize)
     p = (math.ceil(total / pageSize), page)
     if total == 0:
         return dict(total=total, page=p, list=())
-    incomes = await Income.findAll(orderBy='id desc', where=where, limit=limit)
+    try:
+        incomes = await Income.findAll(orderBy='id desc', where=where, limit=limit)
+    except:
+        ValueError("sql error:Income.findAll(orderBy='id desc', where=where, limit=limit)")
+        return {
+            "page": (1, 1),
+            "list": [{"id": "error"}]
+        }
     L = []
     d = {}
     for income in incomes:
         d = {"lookid":income["id"],"date":income["aff_date"],"income":income["money"],"status":income["status"]}
         whereC = "id = %s" %(income["client_id"])
-        clients = await Client.findAll(whereC)
+        try:
+            clients = await Client.findAll(whereC)
+        except:
+            ValueError("sql error:Client.findAll(whereC)")
+            return {
+                "page": (1, 1),
+                "list": [{"id": "error"}]
+            }
         d['channel'] = clients[0]["name"]
 
 
         whereI = "income_id=%s" %(income["id"])
-        settles = await Settlement.findAll(whereI)
+        try:
+            settles = await Settlement.findAll(whereI)
+        except:
+            ValueError("sql error:Settlement.findAll(whereI)")
+            return {
+                "page": (1, 1),
+                "list": [{"id": "error"}]
+            }
         d["incomeM"] = settles[0]["balance"]
         d["percentage"] = str("%2.3f" %(d["incomeM"]/income["money"])) + "%"
 
@@ -49,17 +77,38 @@ async def settleApply_formIndex(*,keyword=None,action=None, page=1, pageSize=10)
     pageSize = int(pageSize)
 
     wherei = "id = %s" % (keyword)
-    incomes = await Income.findAll(where=wherei)
+    try:
+        incomes = await Income.findAll(where=wherei)
+    except:
+        ValueError("sql error:Income.findAll(where=wherei)")
+        return {
+            "page": (1, 1),
+            "list": [{"name": "error"}]
+        }
     client_id = incomes[0]["client_id"]
 
     wherec = "id = %s" %(client_id)
-    clients = await Client.findAll(wherec=wherec)
+    try:
+        clients = await Client.findAll(wherec=wherec)
+    except:
+        ValueError("sql error:Client.findAll(wherec=wherec)")
+        return {
+            "page": (1, 1),
+            "list": [{"name": "error"}]
+        }
 
     name = clients[0]["name"]
     limit = ((page - 1) * pageSize, pageSize)
 
     wheres = "client_id = %s" % (client_id)
-    settles = await Settlement.findAll(orderBy='id desc',where=wheres,limit=limit)
+    try:
+        settles = await Settlement.findAll(orderBy='id desc',where=wheres,limit=limit)
+    except:
+        ValueError("sql error:Settlement.findAll(orderBy='id desc',where=wheres,limit=limit)")
+        return {
+            "page": (1, 1),
+            "list": [{"name": "error"}]
+        }
     settles = obj2str(settles)
     total = len(settles)
     p = (math.ceil(total / pageSize), page)
@@ -85,7 +134,13 @@ async def settleApply_formIndex(*,keyword=None,action=None, page=1, pageSize=10)
 @get('/settleApply_identify')
 async def settleApply_identify(*,id):
     wherei = "id=%s" %(id)
-    incomes = await Income.findAll(where=wherei)
+    try:
+        incomes = await Income.findAll(where=wherei)
+    except:
+        ValueError("sql error:Income.findAll(where=wherei)")
+        return {
+            'msg': '操作失败'
+        }
     incomes[0]["status"] = 2
     incomes= obj2str(incomes)
     #time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
