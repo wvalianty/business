b569ef1042fcb5504a79100f89e9a178bdd08126
root <root@DESKTOP-ETNKUVH.localdomain>
2018-04-25 09:40:10 +0800

    建表修改

--- backup/business.sql
+++ backup/business.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 SQLyog Ultimate v11.33 (64 bit)
 MySQL - 5.5.53 : Database - business
@@ -14,7 +13,7 @@ MySQL - 5.5.53 : Database - business
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`business` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `business` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `business`;
 
@@ -107,32 +106,31 @@ CREATE TABLE `settlement` (
 
 /*Table structure for table `syslog` */
 
-DROP TABLE IF EXISTS `syslog`;
-
+DROP TABLE IF EXISTS `users`;
 create table users (
-    `id` varchar(50) not null,
-    `phoneN` varchar(50) not null,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `phone` varchar(50) not null,
     `email` varchar(50) not null,
     `passwd` varchar(50) not null,
-    `role` tinyint(4) not null,
+    `role` tinyint(4) not null COMMENT '0:管理员,1:运营侧,2:财务侧',
     `name` varchar(50) not null,
     `created_at` datetime not null,
-    `admin`  tinyint(4)  DEFAULT 0 COMMENT '是否为管理员',
     unique key `idx_email` (`email`),
     key `idx_created_at` (`created_at`),
     primary key (`id`)
 ) engine=innodb default charset=utf8;
 
+DROP TABLE IF EXISTS `syslog`;
+
 CREATE TABLE `syslog` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `uid`  varchar(50)  NOT NULL  COMMENT '用户id',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL  COMMENT '用户',
+  `affetced_id`  int(11) DEFAULT NULL COMMENT '受影响id',
   `operate` varchar(10) NOT NULL DEFAULT '' COMMENT '管理员行为:add,update,delete',
   `table` varchar(50) NOT NULL COMMENT '管理员操作的表名，也可以理解为模块',
   `module` varchar(50) DEFAULT '' COMMENT '操作模块',
   `sql` text NOT NULL COMMENT '管理员操作的SQL',
-  `datetime` datetime NOT NULL COMMENT '操作时间',
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`uid`),
-  CONSTRAINT `syslog_user_id` FOREIGN KEY (`uid`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=451 DEFAULT CHARSET=utf8;
+  `add_date` datetime NOT NULL COMMENT '操作时间',
+  PRIMARY KEY (`id`)
+) engine=innodb default charset=utf8;
 
