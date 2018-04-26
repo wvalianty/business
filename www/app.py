1fc622562c127352175daeeaea7cc5118e28080d
root <root@DESKTOP-ETNKUVH.localdomain>
2018-04-26 11:16:29 +0800

    添加登出设置

--- www/app.py
+++ www/app.py
@@ -29,8 +29,8 @@ logging.basicConfig(
 #1、运营侧
 #2、财务侧
 
-finance = set(["/board","/apis/finish","/apis/main/index","/","/apis/board/index","/apis/invoiceApply_index/index","/apis/settleApply_index/index","/apis/settleApply_look/look","/settleApply_identify","/invoiceApply_index","/settleApply_index","/settleApply_look","/login/index","/main"])
-operate = set(["/apis/income/formInit","/apis/income/getIncomeId","/apis/income/form","/apis/invoice/formInit","/invoice/form","/apis/business/index","/apis/settlement/del","/apis/settlement/form","/apis/settlement/formInit","/apis/settlement/info","/apis/settlement/index","/settlement","/apis/business/index","/apis/business/form","/apis/client/form","/apis/client/index","/apis/invoice/index","/invoice","/","/client","/client/form","/apis/income/index","/business","/business/form","/income","/income/form","/login/index","/main"])
+finance = set(["/signout","/board","/apis/finish","/apis/main/index","/","/apis/board/index","/apis/invoiceApply_index/index","/apis/settleApply_index/index","/apis/settleApply_look/look","/settleApply_identify","/invoiceApply_index","/settleApply_index","/settleApply_look","/login/index","/main"])
+operate = set(["/signout","/apis/income/formInit","/apis/income/getIncomeId","/apis/income/form","/apis/invoice/formInit","/invoice/form","/apis/business/index","/apis/settlement/del","/apis/settlement/form","/apis/settlement/formInit","/apis/settlement/info","/apis/settlement/index","/settlement","/apis/business/index","/apis/business/form","/apis/client/form","/apis/client/index","/apis/invoice/index","/invoice","/","/client","/client/form","/apis/income/index","/business","/business/form","/income","/income/form","/login/index","/main"])
 manager = set(["/apis/manager/del","/apis/board/index","/syslog","/apis/manager/index","/apis/manager/info","/apis/manager/form","/api/login","/manager/edit","/manager/form","/manager"])
 super_user = finance | operate
 super_user = super_user | manager
--- www/controllers/login.py
+++ www/controllers/login.py
@@ -165,13 +165,13 @@ async def delete(*, id):
 
 
 
-# @get('/signout')
-# def signout(request):
-#     referer = request.headers.get('Referer')
-#     r = web.HTTPFound(referer or '/')
-#     r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
-#     logging.info('user signed out.')
-#     return r
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
 
 # _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 # _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
