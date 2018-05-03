8602a4bf22636e033cd9d5df09a325ed20ae5e7e
root <root@DESKTOP-ETNKUVH.localdomain>
2018-05-03 12:08:23 +0800

    结算申请修改

--- www/controllers/settleApply.py
+++ www/controllers/settleApply.py
@@ -1,5 +1,5 @@
 from core.coreweb import get, post
-from lib.models import Income, Settlement,Client
+from lib.models import Income, Settlement,Client,Invoice
 import math,datetime
 from lib.common import obj2str
 
@@ -7,13 +7,13 @@ from lib.common import obj2str
 async def settleApply_index(*,keyword=None, page=1, pageSize=10):
     page = int(page)
     pageSize = int(pageSize)
-    where = ' where 1 = 1 and settle.is_delete = 0 and inc.is_delete = 0 and c.is_delete = 0 '
+    where = ' where 1 = 1 and inc.status = 2 and settle.is_delete = 0 and inc.is_delete = 0 and c.is_delete = 0 '
     #if search
     # if keyword:
     #     where = "name like '%%{}%%'".format(keyword)
 
     # count_id = '%s' %("settle.id")
-    sql_total = 'select count(*) co  from settlement settle inner join income inc on settle.income_id = inc.id inner join client c  on settle.client_id = c.id %s ' %(where)
+    sql_total = 'select count(*) co  from settlement settle left join income inc on settle.income_id = inc.id inner join client c  on settle.client_id = c.id %s ' %(where)
     try:
         reL = await Settlement.query(sql_total)
         if reL[0]["co"] == 0:
@@ -27,7 +27,7 @@ async def settleApply_index(*,keyword=None, page=1, pageSize=10):
         return dict(total=total, page=(0,0), list=())
     limit = ((page - 1) * pageSize, pageSize)
     p = (math.ceil(total / pageSize), page)
-    sql_res = 'select inc.id,inc.income_id,c.name,inc.aff_date,inc.money,inc.status,settle.balance,settle.status sstatus from settlement settle inner join income inc on settle.income_id = inc.id inner join client c  on settle.client_id = c.id where settle.is_delete = 0 and inc.is_delete = 0 and c.is_delete = 0  order by settle.id desc limit %s,%s' %(limit[0],limit[1])
+    sql_res = 'select inc.id,inc.income_id,c.name,inc.aff_date,inc.money,inc.status,settle.balance,settle.status sstatus from settlement settle left join income inc on settle.income_id = inc.id inner join client c  on settle.client_id = c.id where settle.is_delete = 0 and inc.is_delete = 0 and inc.status = 2 and c.is_delete = 0  order by settle.id desc limit %s,%s' %(limit[0],limit[1])
     try:
         res = await Settlement.query(sql_res)
     except:
@@ -38,10 +38,10 @@ async def settleApply_index(*,keyword=None, page=1, pageSize=10):
             res[i]["percentage"] = '%5.2f' %(res[i]["balance"]/res[i]["money"]*100)
             res[i]["percentage"] = str(res[i]["percentage"]) + "%"
         for j in range(len(res)):
-            if res[j]["sstatus"] == 0:
+            if res[j]["sstatus"] == 1:
                 t = res[j]
                 res.pop(j)
-                res.insert(0, t)
+                res.append(t)
     except:
         return dict(total=total, page=p, list=())
     return {
@@ -52,50 +52,74 @@ async def settleApply_index(*,keyword=None, page=1, pageSize=10):
 
 
 
-#keyword 就是lookid  income.id
+#keyword 就是 income.id
 @get('/apis/settleApply_look/look')
-async def settleApply_formIndex(*,keyword=None,action=None, page=1, pageSize=10):
+async def settleApply_formIndex(*,keyword=None, page=1, pageSize=10):
     page = int(page)
     pageSize = int(pageSize)
-
     income_id = int(keyword)
-    sql_total = 'select count(*) co from settlement settle inner join income inc on settle.income_id = inc.id  inner join client c on inc.client_id = c.id where settle.income_id = %s and c.is_delete = 0 and inc.is_delete = 0 and settle.is_delete = 0 and settle.client_id = c.id ' %(income_id)
-    re = await  Client.query(sql_total)
-    try:
-        reL = await  Client.query(sql_total)
-        total = reL[0]["co"]
-        if total == 0:
-            return dict(total=total, page=(0,0), list=())
-    except:
-        raise ValueError("查询数据库错误,/apis/settleApply_look/look")
-    p = (math.ceil(total / pageSize), page)
 
-    limit = ((page - 1) * pageSize, pageSize)
-    sql_res = ' select c.name,settle.balance,inc.status,inc.add_date from settlement settle inner join income inc on settle.income_id = inc.id  inner join client c on inc.client_id = c.id where settle.income_id = %s and c.is_delete = 0 and inc.is_delete = 0 and settle.is_delete = 0 and settle.client_id = c.id order by inc.id desc  limit %s,%s' %(income_id,limit[0],limit[1])
+    print(income_id)
+
+    total = 1
+    p = (1, 1)
+
+    where = " income_id = %s" %(income_id)
     try:
-        res = await  Client.query(sql_res)
+        res = await Invoice.findAll(where=where)
         res = obj2str(res)
     except:
-        raise ValueError("查询数据库错误,/apis/settleApply_look/look")
+        raise ValueError("/apis/settleApply_look/look  数据出现错误")
+    print(res)
+
     return {
         "total": total,
         "page": p,
         "list": res
     }
+    # wherei = " id = %s " %(income_id)
+    # try:
+    #     incomes = await Income.findAll(where=wherei)
+    #     incomes = obj2str(incomes)
+    # except:
+    #     raise ValueError("/apis/settleApply_look/look查询数据库失败")
+    # client_id = incomes[0]["client_id"]
+    #
+    #
+    # sql_total = 'select count(*) co from settlement settle inner join income inc on settle.income_id = inc.id  inner join client c on inc.client_id = c.id where settle.income_id = %s and c.is_delete = 0 and inc.is_delete = 0 and settle.is_delete = 0 and settle.client_id = c.id ' %(income_id)
+    # re = await  Client.query(sql_total)
+    # try:
+    #     reL = await  Client.query(sql_total)
+    #     total = reL[0]["co"]
+    #     if total == 0:
+    #         return dict(total=total, page=(0,0), list=())
+    # except:
+    #     raise ValueError("查询数据库错误,/apis/settleApply_look/look")
+    # p = (math.ceil(total / pageSize), page)
+    #
+    # limit = ((page - 1) * pageSize, pageSize)
+    # sql_res = ' select c.name,settle.balance,inc.status,inc.add_date from settlement settle inner join income inc on settle.income_id = inc.id  inner join client c on inc.client_id = c.id where settle.income_id = %s and c.is_delete = 0 and inc.is_delete = 0 and settle.is_delete = 0 and settle.client_id = c.id order by inc.id desc  limit %s,%s' %(income_id,limit[0],limit[1])
+    # try:
+    #     res = await  Client.query(sql_res)
+    #     res = obj2str(res)
+    # except:
+    #     raise ValueError("查询数据库错误,/apis/settleApply_look/look")
+
 
 #id是收入id
 @get('/settleApply_identify')
 async def settleApply_identify(*,id):
-    wheres = "income_id=%s" %(id)
+    wheres = " income_id=%s " %(id)
     try:
         settles = await Settlement.findAll(where=wheres)
+        settles = obj2str(settles)
     except:
-        ValueError("sql error:Income.findAll(where=wherei)")
+        ValueError("sql error:Income.findAll(where=wheres)")
         return {
             'msg': '操作失败'
         }
     settles[0]["status"] = 1
-    settles= obj2str(settles)
+    settles[0]["finished_time"] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     rs = await Settlement(**settles[0]).update()
     if rs == 1:
         return {
